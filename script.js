539e96f860210c02a9fc5dca6c822418e777cb19
Violetta Lomako <violetta.lomako@gmail.com>
2020-11-21 23:50:47 +0300

    Второй урок

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-let money
-let income
-let addExpenses
-let deposit
-let mission
-let period
+let money = 70000
+let income = 'репетитор'
+let addExpenses = 'интернет, ипотека, продукты'
+let deposit = true
+let mission = 100000
+let period = 7
+let budgetDay = money / 30
 
-alert("Hello world!");
-console.log('Hello world!');
+console.log(typeof money);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев')
+console.log('Цель заработать ' + mission + ' рублей')
+console.log(addExpenses.toLowerCase().split(', '));
+console.log(budgetDay);
