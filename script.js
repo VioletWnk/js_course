2481dee46a7820c5d6f209556721f579d89cf46a
Violetta Lomako <violetta.lomako@gmail.com>
2020-11-26 23:02:29 +0300

    Четвертый урок

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+'use strict';
+
+
+//lesson01
 let money = 70000
 let income = 'репетитор'
 let addExpenses = 'интернет, ипотека, продукты'
@@ -6,15 +10,17 @@ let mission = 100000
 let period = 7
 let budgetDay = money / 30
 
+
+//lesson02
 console.log(typeof money);
-console.log(addExpenses.length);
-console.log('Период равен ' + period + ' месяцев')
-console.log('Цель заработать ' + mission + ' рублей')
-console.log(addExpenses.toLowerCase().split(', '));
-console.log(budgetDay);
+// console.log(addExpenses.length);
+// console.log('Период равен ' + period + ' месяцев')
+// console.log('Цель заработать ' + mission + ' рублей')
+// console.log(addExpenses.toLowerCase().split(', '));
+// console.log(budgetDay);
 
-'use strict';
 
+//lesson03
 money = +prompt('Ваш месячный доход?');
 addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
 deposit = confirm('Есть ли у вас депозит в банке?');
@@ -25,12 +31,10 @@ let amount1 = +prompt('Во сколько это обойдется?');
 let expenses2 = prompt('Введите обязательную статью расходов?');
 let amount2 = +prompt('Во сколько это обойдется?');
 
-let budgetMonth = amount1 + amount2;
-console.log('Бюджет на месяц: ' + budgetMonth);
-
-console.log('Цель бюдет достигнута за: ' + Math.round(mission / (money - budgetMonth)));
-budgetDay = budgetMonth / 30
-console.log('Бюджет на день: ' + Math.ceil(budgetDay));
+// console.log('Бюджет на месяц: ' + budgetMonth);
+// console.log('Цель бюдет достигнута за: ' + Math.round(mission / (budgetMonth)));
+// budgetDay = budgetMonth / 30
+// console.log('Бюджет на день: ' + Math.ceil(budgetDay));
 
 if (budgetDay > 1200) {
     console.log('У вас высокий уровень дохода');
@@ -45,3 +49,28 @@ if (budgetDay > 1200) {
         console.log('Что то пошло не так');
     }
 
+
+    
+//lesson04
+
+function getExpensesMonth(){ 
+    return amount1 + amount2;
+}
+console.log(getExpensesMonth()); //Расходы за месяц вызов getExpensesMonth
+
+
+function getAccumulatedMonth(){
+    return money - (amount1 + amount2);
+}
+let accumulatedMonth = getAccumulatedMonth();
+
+
+function getTargetMonth(){
+    return mission / accumulatedMonth;
+}
+console.log(getTargetMonth()); // Cрок достижения цели в месяцах (результат вызова функции getTargetMonth) 
+
+
+budgetDay = accumulatedMonth / 30 
+console.log(budgetDay); //Бюджет на день (budgetDay)
+console.log(addExpenses.split()); //Вывод возможных расходов в виде массива (addExpenses)
